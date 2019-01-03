3006aae4af366a676422c69b465956458bb5bf8c
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-01-03 12:11:28 -0500

    dropped DB to add a dependancy to allow deletion of the joinedtable.

--- db/migrate/20181221205211_create_join_table_student_cohort.rb
+++ db/migrate/20181221205211_create_join_table_student_cohort.rb
@@ -2,7 +2,7 @@ class CreateJoinTableStudentCohort < ActiveRecord::Migration[5.2]
   def change
     create_join_table :students, :cohorts do |t|
       t.references :cohort, foreign_key: true
-      t.references :student, foreign_key: true
+      t.references :student, foreign_key: true, dependent: :destroy
     end
   end
 end
