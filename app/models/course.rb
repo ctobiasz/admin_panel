1b8c968e1fe90eabd6e3ba8541398ad28f52ba21
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-01-10 18:58:54 -0500

    greater_than: 0

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,9 @@
 class Course < ApplicationRecord
   validates :name,  presence: true
-  validates :hours, presence: true
+  validates :hours, presence: true, numericality: { greater_than: 0 }
 
   has_many :cohorts
-  
+
   has_one_attached :avatar
 
 
